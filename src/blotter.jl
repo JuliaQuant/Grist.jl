cd78c2a16f15bdf10b990674511bfe95556c6132
milktrader <milktrader@gmail.com>
2015-03-14 14:03:20 -0400

    add pretty show for transactions

--- src/blotter.jl
+++ src/blotter.jl
@@ -3,4 +3,5 @@ typealias Blotter{T<:AssetTransaction} Timestamp{T}
 #function show(io::IO, b::Array{Blotter})
 function show(io::IO, b::Blotter)
     print(io, "          ", "Qty", "Fill Price")
+    print(io, b.value.quantity, b.value.price)
 end
--- src/transaction.jl
+++ src/transaction.jl
@@ -5,5 +5,28 @@ immutable AssetTransaction
 end
 
 function show(io::IO, at::AssetTransaction)
-    print(io, string(at.quantity) * "units", "    ", string(at.price) * "   " * string(at.asset.currency), "     ", at.asset.ticker)
+ 
+    # unit variable
+    if typeof(at.asset) == Stock || typeof(at.asset) == LongStock || typeof(at.asset) == ShortStock 
+        unit = "shares"
+    else
+        unit = "contracts"
+    end
+
+    # currency variable
+    if at.asset.currency == USD
+        cvar = "\$"
+    elseif at.asset.currency == EUR
+        cvar = "€"
+    elseif at.asset.currency == GBP
+        cvar = "£"
+    elseif at.asset.currency == JPY
+        cvar = "¥"
+    elseif at.asset.currency == AUD
+        cvar = "AU\$"
+    else
+        cvar = "" 
+    end
+
+    print(io, string(at.quantity) * " " * unit, "    ",   cvar * " " * string(at.price), "     ", at.asset.ticker)
 end
