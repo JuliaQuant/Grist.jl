0ce074a240ab49864a4294764028a1aff41bd823
milktrader <milktrader@gmail.com>
2015-03-14 21:03:18 -0400

    more pretty now with alignment logic

--- src/blotter.jl
+++ src/blotter.jl
@@ -1,7 +1,7 @@
 typealias Blotter{T<:AssetTransaction} Timestamp{T}
 
-#function show(io::IO, b::Array{Blotter})
 function show(io::IO, b::Blotter)
-    print(io, "          ", "Qty", "Fill Price")
-    print(io, b.value.quantity, b.value.price)
+    print_with_color(:blue, io, string(b.timestamp))
+    print_with_color(:magenta, io, " | ")
+    print_with_color(:blue, io, string(b.value))
 end
--- src/transaction.jl
+++ src/transaction.jl
@@ -5,7 +5,7 @@ immutable AssetTransaction
 end
 
 function show(io::IO, at::AssetTransaction)
- 
+    
     # unit variable
     if typeof(at.asset) == Stock || typeof(at.asset) == LongStock || typeof(at.asset) == ShortStock 
         unit = "shares"
@@ -24,9 +24,59 @@ function show(io::IO, at::AssetTransaction)
         cvar = "¥"
     elseif at.asset.currency == AUD
         cvar = "AU\$"
+    elseif at.asset.currency == NZD
+        cvar = "NZ\$"
     else
         cvar = "" 
     end
 
-    print(io, string(at.quantity) * " " * unit, "    ",   cvar * " " * string(at.price), "     ", at.asset.ticker)
+    if at.quantity >= 0 && unit == "shares"
+        print_with_color(:green, io, " " * string(at.quantity) * "  ")
+        print_with_color(:blue, io, unit * ^(" ", 5))
+        at.price > 1000 ?
+            print_with_color(:blue, io, string(at.price) * ^(" ", 2)) :
+        at.price > 100 ?
+            print_with_color(:blue, io, ^(" ", 1) * string(at.price) * ^(" ", 2)) :
+        at.price > 10 ?
+            print_with_color(:blue, io, ^(" ", 2) * string(at.price) * ^(" ", 2)) :
+            print_with_color(:blue, io, ^(" ", 3) * string(at.price) * ^(" ", 2))
+        print_with_color(:blue, io, cvar * "  ")
+        print_with_color(:blue, io, string(at.asset.ticker) * "  ")
+    elseif at.quantity >= 0 && unit == "contracts"
+        print_with_color(:green, io, " " * string(at.quantity) * "  ")
+        print_with_color(:blue, io, unit * ^(" ", 2))
+        at.price > 1000 ?
+            print_with_color(:blue, io, string(at.price) * ^(" ", 2)) :
+        at.price > 100 ?
+            print_with_color(:blue, io, ^(" ", 1) * string(at.price) * ^(" ", 2)) :
+        at.price > 10 ?
+            print_with_color(:blue, io, ^(" ", 2) * string(at.price) * ^(" ", 2)) :
+            print_with_color(:blue, io, ^(" ", 3) * string(at.price) * ^(" ", 2))
+        print_with_color(:blue, io, cvar * "  ")
+        print_with_color(:blue, io, string(at.asset.ticker) * "  ")
+    elseif unit == "shares"
+        print_with_color(:red, io, string(at.quantity) * "  ")
+        print_with_color(:blue, io, unit * ^(" ", 5))
+        at.price > 1000 ?
+            print_with_color(:blue, io, string(at.price) * ^(" ", 2)) :
+        at.price > 100 ?
+            print_with_color(:blue, io, ^(" ", 1) * string(at.price) * ^(" ", 2)) :
+        at.price > 10 ?
+            print_with_color(:blue, io, ^(" ", 2) * string(at.price) * ^(" ", 2)) :
+            print_with_color(:blue, io, ^(" ", 3) * string(at.price) * ^(" ", 2))
+        print_with_color(:blue, io, cvar * "  ")
+        print_with_color(:blue, io, string(at.asset.ticker) * "  ")
+    else # only contracts left here
+        print_with_color(:red, io, string(at.quantity) * "  ")
+        print_with_color(:blue, io, unit * ^(" ", 2))
+        at.price > 1000 ?
+            print_with_color(:blue, io, string(at.price) * ^(" ", 2)) :
+        at.price > 100 ?
+            print_with_color(:blue, io, ^(" ", 1) * string(at.price) * ^(" ", 2)) :
+        at.price > 10 ?
+            print_with_color(:blue, io, ^(" ", 2) * string(at.price) * ^(" ", 2)) :
+            print_with_color(:blue, io, ^(" ", 3) * string(at.price) * ^(" ", 2))
+        print_with_color(:blue, io, cvar * "  ")
+        print_with_color(:blue, io, string(at.asset.ticker) * "  ")
+    end
 end
