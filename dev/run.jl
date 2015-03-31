b1a30a736dbdb280da9a18bbb7363b3a51ad81de
milktrader <milktrader@gmail.com>
2015-03-30 20:17:03 -0400

    finished functioning simulate method

--- dev/run.jl
+++ dev/run.jl
@@ -1,21 +1,37 @@
 using MarketData
+export simulate
 
 # run model without closing price updates
 
-for i in 1:length(res)
-    push!(trans, res[i].value)
+# convert TimeArray to Timestamps array
+clt = Timestamp[]
+
+for i in 1:length(cl)
+    push!(clt, Timestamp(cl[i].timestamp[1], cl[i].values[1])) 
 end
 
-# run model with closing price updates
+cr = vcat(res, clt) # combine both arrays with txn array first
+sd = sort(Date[c.timestamp for c in cr]) # get dates and sort
+ud = unique(sd) # remove duplicates
 
-# bstamp = [b.timestamp for b in res]
-# 
-# for i in 1:length(cl)
-#     if cl.timestamp[i] in bstamp
-#         idx = findfirst(bstamp, cl.timestamp[i])
-#         push!(trans, res[idx].value)
-#         push!(mark_price, cl.values[i])
-#     else
-#         push!(mark_price, cl.values[i])
-#     end
-# end
+mixed_array = cr[ud[1]] # initialize array to be pushed
+
+for i in 2:length(ud)
+    mixed_array = vcat(mixed_array, cr[ud[i]])
+end
+
+# this takes mixed_array and updates the following
+# txn & mark_price explicity
+# A, Pval, Aval by association
+
+function simulate(tsv::Vector{Timestamp})
+    for t in 1:length(tsv)
+        if typeof(tsv[t].value) <: FinancialAsset
+            push!(txn, tsv[t].value)
+        elseif typeof(tsv[t].value) == Float64
+            push!(mark_price, tsv[t].value)
+        else
+            error("rogue timestamp in your midst!")
+        end
+    end
+end
